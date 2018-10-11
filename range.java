27dd239c812bb29d5c280b4e19522350d7e939ff
ARAVINDH17 <35103867+ARAVINDH17@users.noreply.github.com>
2018-10-11 18:39:07 +0530

    Add files via upload

---
+++ range.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+		long x=sc.nextLong();
+		long y=sc.nextLong();
+		for(long i=x+1;i<y;i++){
+			if(i%2!=0)
+			System.out.print(i+" ");
+		}
+    
+  }
+}
