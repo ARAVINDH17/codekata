06610c87cfe72372536ded7af535adca4284a54e
ARAVINDH17 <35103867+ARAVINDH17@users.noreply.github.com>
2018-10-11 14:22:28 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,14 +3,16 @@ class Main {
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		if(n==0){
-			System.out.println("Zero");
-		}
-		else if(n>0){
-			System.out.println("Positive");
-		}
-		else{
-			System.out.println("Negative");
-		}
+	   if(n<0){
+			 System.out.println("invalid");
+		 }
+		 else{
+			 if(n%2==0){
+				 System.out.println("Even");
+			 }
+			 else{
+				 System.out.println("Odd");
+			 }
+		 }
   }
 }
