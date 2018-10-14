c503982f3aa60f6dd4258fdb27f3e4a3b95c26b2
ARAVINDH17 <35103867+ARAVINDH17@users.noreply.github.com>
2018-10-14 18:50:04 +0530

    Add files via upload

---
+++ duplicatearr1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int a[]=new int[n];
+    for(int i=0;i<n;i++)
+    a[i]=sc.nextInt();
+    Arrays.sort(a);
+    String tem="";
+    int count=0;
+    for(int i=1;i<n;i++){
+         if(a[i-1]!=a[i])
+         count=0;
+         if(a[i-1]==a[i])
+         count++;
+         if(count==1)
+         tem=tem+" "+Integer.toString(a[i]);
+    }
+	if(tem.length()!=0){
+    tem=tem.trim();
+    System.out.println(tem);
+	}
+	else{
+		System.out.println("unique");
+  }
+}
+}
