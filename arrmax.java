5e043647fd485f1d2f0ed1deb22136691b495c7d
ARAVINDH17 <35103867+ARAVINDH17@users.noreply.github.com>
2018-10-14 07:12:57 +0530

    Add files via upload

---
+++ arrmax.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a[]=new int[10];
+    int max=0;
+    for(int i=0;i<10;i++){
+      a[i]=sc.nextInt();
+      if(a[i]>max)
+      max=a[i];
+    }
+    System.out.println(max);
+  }
+}
