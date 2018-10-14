6e929d01f2efb0790a7fd48d486b37115494ea4d
ARAVINDH17 <35103867+ARAVINDH17@users.noreply.github.com>
2018-10-14 07:29:37 +0530

    Add files via upload

---
+++ factnew.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int x=1,y=1;
+    System.out.print("1"+""+"1");
+    for(int i=2;i<n;i++){
+      int c=x+y;
+      System.out.print(c);
+      x=y;
+      y=c;
+    }
+  }
+}
