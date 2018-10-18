b977091ebe3747c79d01c07159590d20629b63ae
ARAVINDH17 <35103867+ARAVINDH17@users.noreply.github.com>
2018-10-18 22:21:04 +0530

    Add files via upload

---
+++ mulong.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+	  long x=sc.nextLong();
+		long y=sc.nextLong();
+		long z=x*y;
+		System.out.print(Long.toString(z));
+
+  }
+}
