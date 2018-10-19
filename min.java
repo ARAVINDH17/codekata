1f302ce24b1c9ae49d2b7b178cc70070286e34f6
ARAVINDH17 <35103867+ARAVINDH17@users.noreply.github.com>
2018-10-19 09:49:55 +0530

    Add files via upload

---
+++ min.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		int max=0;
+		for(int i=0;i<n;i++){
+		a[i]=sc.nextInt();
+		if(i==0)
+		max=a[i];
+		else{
+		if(max>a[i])
+		max=a[i];
+		}
+		}
+		System.out.print(max);
+		 
+
+  }
+}
