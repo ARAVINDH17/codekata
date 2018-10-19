6f4f2b77ae4646c7359e400bee410734d7ed1899
ARAVINDH17 <35103867+ARAVINDH17@users.noreply.github.com>
2018-10-19 12:33:24 +0530

    Add files via upload

---
+++ duuup.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    String s=sc.next();
+    String tem=""+s.charAt(0);
+    for(int i=1;i<s.length();i++){
+      boolean bol=true;
+      for(int j=i-1;j>=0;j--){
+        if(s.charAt(i)==s.charAt(j))
+        bol=false;
+      }
+      if(bol)
+      tem=tem+""+s.charAt(i);
+    }
+    System.out.print(tem.trim());
+  }
+}
