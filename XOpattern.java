07f6add66ca533689dedbe326ed65924a5a2a7dd
ARAVINDH17 <35103867+ARAVINDH17@users.noreply.github.com>
2019-02-25 19:15:52 +0530

    Add files via upload

---
+++ XOpattern.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class XO_pattern {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			char arr[][]=new char[n][n];
+			int temp=n;
+			char c='*';
+			for(int i=0;i<n/2;i++) {
+			 c=(i%2==0)?'X':'O';
+			    assign(c,temp,i,arr);
+			    temp=temp-2;
+			}
+			if(n%2!=0) {
+				char matrix_mid_value=(c=='X')?'O':'X';
+				arr[n/2][n/2]= matrix_mid_value;
+			}
+			printMatrix(arr);
+			sc.close();
+	}
+	public static void assign(char x,int temp,int i,char arr[][]) {
+		int a=0;
+		for(a=i;a<temp+i;a++){
+			arr[i][a]=x;}
+		
+		for(int b=i;b<temp+i;b++){
+         arr[a-1][b]=x;
+		}
+	
+		for(int c=i;c<temp+i;c++){
+			arr[c][i]=x;
+		}
+		
+	
+		for(int d=i;d<temp+i;d++){
+			arr[d][a-1]=x;
+		}
+		
+	}
+	public static void printMatrix(char arr[][]) {
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
