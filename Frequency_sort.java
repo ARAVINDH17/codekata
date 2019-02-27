0f5725ab6c6081f8e1c38f2281761e5800e67b9f
ARAVINDH17 <35103867+ARAVINDH17@users.noreply.github.com>
2019-02-27 21:56:12 +0530

    Add files via upload

---
+++ Frequency_sort.java
@@ -0,0 +1,61 @@
+import java.util.*;
+public class sort_element_usingFac {
+
+
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     int a[]=new int[n];
+     for(int i=0;i<n;i++){
+         a[i]=sc.nextInt();
+     }
+     for(int i=0;i<n;i++){
+         for(int j=i+1;j<n;j++){
+             if(a[i]>a[j]){
+                 int temp=a[i];
+                 a[i]=a[j];
+                 a[j]=temp;
+             }
+         }
+     }
+    int subarr=1;
+    for(int i=1;i<n;i++){
+        if(a[i-1]!=a[i])
+            subarr++;
+    }
+   
+   int b[][]=new int[subarr][2];
+   int count=0,k=0;
+   for(int i=0;i<n;i++) {
+	   count++;
+	  if(i==n-1||a[i]!=a[i+1]) {
+		  b[k][0]=a[i];
+		  b[k][1]=count;
+		  k++;
+		  count=0;
+	  }
+   }
+   for(int i=0;i<subarr;i++) {
+	   for(int j=i+1;j<subarr;j++) {
+		   if(b[i][1]<b[j][1]) {
+			   int tem=b[i][1];
+			   b[i][1]=b[j][1];
+			   b[j][1]=tem;
+			   int tem1=b[i][0];
+			   b[i][0]=b[j][0];
+			   b[j][0]=tem1;
+		   }
+	   }
+   }
+   
+  
+    for(int i=0;i<subarr;i++) {
+    	for(int j=0;j<b[i][1];j++) {
+    		System.out.print(b[i][0]+" ");
+    	}
+    
+    }
+    
+    } 
+
+}
