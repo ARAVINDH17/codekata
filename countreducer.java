872788dc7723571b12b4c1246bab1e477f51e5c7
Aravindh B <35103867+ARAVINDH17@users.noreply.github.com>
2019-07-10 07:43:22 +0530

    Update countreducer.java

--- countreducer.java
+++ countreducer.java
@@ -14,20 +14,41 @@ public class Main {
 	 }
   }
 	public static boolean check(long n,long a,long b){
-  int k=0;
-	long tem=n;
-	while(tem>0){
-		if(k%2==0){
-			tem=tem-a;
-		}
-		else{
-			tem=tem-b;
-			if(tem==0)
-			return true;
-		}
+	  long tem=n;
+		int k=0,p=0,q=0;
+		long sum1=0,sum2=0;
+		while(tem>0){
+			if(k%2==0){
+         if(p%2==0){
+					 sum1=sum1+a;
+				 }
+				 else{
+					 sum1=sum1+b;
+				 }
+        p++;
+			}
+			else{
+				 if(q%2==0){
+					 sum2=sum2+a;
+				 }
+				 else{
+					 sum2=sum2+b;
+				 }
+        q++;
 
-k++;
-	}
+			}
+			long x=sum1+sum2;
+			if(sum1==sum2&&x==n){
+				return true;
+			}
+			else{
+				if(x>n){
+					return false;
+				}
+			}
+			k++;
+		}
+	
 		return false;
 	}
 }
