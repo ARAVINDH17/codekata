d092c0e1828c82427c29b01ab612c8be27a97dd4
Aravindh B <35103867+ARAVINDH17@users.noreply.github.com>
2019-07-12 05:54:09 +0530

    Add files via upload

---
+++ stringopr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+	  String x=sc.next();
+		String y=sc.next();
+		int count=0;
+		for(int i=0;i<x.length();i++){
+			if(x.charAt(i)==y.charAt(i)){
+			}
+			else{
+				count++;
+			}
+		}
+		if(y.length()>x.length()){
+		count=count+(y.length()-x.length());
+		}
+		System.out.println(count);
+	}
+}
