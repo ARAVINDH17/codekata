84e4c35caac5b2bdb01d278d4757ce478f2ffc2d
Aravindh B <35103867+ARAVINDH17@users.noreply.github.com>
2019-08-06 21:53:38 +0530

    Update 47set.cpp

--- 47set.cpp
+++ 47set.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
   int n;
   cin>>n;
-  int temp;
+  int tem;
   int arr[n];
   for(int i=0;i<n;i++){
     cin>>arr[i];
@@ -11,9 +11,9 @@ int main() {
   for(int i=0;i<n;i++){
     for(int j=i+1;j<n;j++){
       if(arr[i]>arr[j]){
-        temp=arr[i];
+        tem=arr[i];
         arr[i]=arr[j];
-        arr[j]=temp;
+        arr[j]=tem;
       }
     }
   }
