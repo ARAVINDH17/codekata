ec41902dff92a71b9bee285499b56642d5ebba3b
Aravindh B <35103867+ARAVINDH17@users.noreply.github.com>
2019-08-06 15:59:04 +0530

    Add files via upload

---
+++ primeNumberBeg.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		String s="";
+		for(int i=x+1;i<y;i++){
+			boolean bol=true;
+			  for(int j=2;j<=i/2;j++){
+					if(i%j==0)
+           bol=false;
+				}
+				if(bol){
+					s=s+" "+i;
+				}
+		}
+		s=s.trim();
+		System.out.print(s);
+  }
+}
