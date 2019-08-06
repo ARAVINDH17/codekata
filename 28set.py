f022e22389addd6192853e817f3dba605f93b76d
Aravindh B <35103867+ARAVINDH17@users.noreply.github.com>
2019-08-06 21:32:11 +0530

    Update 28set.py

--- 28set.py
+++ 28set.py
@@ -1,7 +1,7 @@
-a = []
+b = []
 n = int(input())
 for i in range(0,n):
     s = int(input())
-    a.append(s)
+    b.append(s)
 for i in range(0,n):
-    print(str(a[i])+" "+str(i))
+    print(str(b[i])+" "+str(i))
